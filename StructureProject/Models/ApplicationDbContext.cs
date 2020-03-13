6b50e8e8402ecac12a12e22c89e234ed8761318f
Αβραάμ Λιαούτσης <av.liaoutsis@outlook.com>
2020-03-13 21:10:23 +0200

    ApplicationDbContext

---
+++ StructureProject/Models/ApplicationDbContext.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNet.Identity.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace StructureProject.Models
+{
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public ApplicationDbContext()
+            : base("StructureProject", throwIfV1Schema: false)
+        {
+
+        }
+        public DbSet<Pet> Pets { get; set; }
+        public DbSet<Person> Persons { get; set; }
+        public DbSet<ContactInfo> ContactInfos { get; set; }
+        public DbSet<Mail> Mails { get; set; }
+        public DbSet<HostInfo> HostInfos { get; set; }
+        public DbSet<Reservation> Reservations { get; set; }
+
+
+        public static ApplicationDbContext Create()
+        {
+            return new ApplicationDbContext();
+        }
+    }
+
+}
--- StructureProject/Models/IdentityModels.cs
+++ StructureProject/Models/IdentityModels.cs
@@ -43,24 +43,4 @@ namespace StructureProject.Models
 
     }
 
-    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
-    {
-        public ApplicationDbContext()
-            : base("StructureProject", throwIfV1Schema: false)
-        {
-    
-        }
-        public DbSet<Pet> Pets { get; set; }
-        public DbSet<Person> Persons { get; set; }
-        public DbSet<ContactInfo> ContactInfos { get; set; }
-        public DbSet<Mail> Mails { get; set; }
-        public DbSet<HostInfo> HostInfos { get; set; }
-        public DbSet<Reservation> Reservations { get; set; }
-
-
-        public static ApplicationDbContext Create()
-        {
-            return new ApplicationDbContext();
-        }
-    }
 }
